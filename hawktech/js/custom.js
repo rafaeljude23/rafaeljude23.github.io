2278f7d6f64add7c587eb2a0549a9a0855c51585
Rafael Jude Manansala <40167581+rafaeljude23@users.noreply.github.com>
2022-06-12 15:37:06 +0800

    Update hawktech

--- hawktech/js/custom.js
+++ hawktech/js/custom.js
@@ -9,15 +9,28 @@ $(document).ready(function () {
     }
   });
 
+  // $('a[href^="#"]').on('click', function (event) {    
+  //   var target = $(this.getAttribute('href'));    
+  //   if (target.length) {      
+  //     event.preventDefault();      
+  //     $('html, body').stop().animate({        
+  //       scrollTop: target.offset().top      
+  //     }, 1000);    }  
+  // });
 
-  $('a[href^="#"]').on('click', function (event) {    
-    var target = $(this.getAttribute('href'));    
-    if (target.length) {      
-      event.preventDefault();      
-      $('html, body').stop().animate({        
-        scrollTop: target.offset().top      
-      }, 1000);    }  
-  });
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    // document.getElementById("navbar").style.top = "0";
+    $('.navbar').css('top','0');
+  } else  {
+    // document.getElementById("navbar").style.top = "-50px";
+      $('.navbar').css('top', -$('.navbar').outerHeight());
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 
 });
 
