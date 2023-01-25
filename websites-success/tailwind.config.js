db6d36b018f9321f4550d487bb4618b08f4ca1e7
Rafael Jude Manansala <40167581+rafaeljude23@users.noreply.github.com>
2023-01-25 23:20:35 +0800

    initial commit website success

---
+++ websites-success/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'orange': '#d05625',
+        'blue': '#6c97bc',
+        'lightblue': '#cad5dd',
+        'brown': '#561708',
+      },
+    },
+  },
+  plugins: [],
+}
